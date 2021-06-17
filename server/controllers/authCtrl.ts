fe4b1cecd53b4994465c98b5065019955bf8fd13
unknown <ldandoy@overconsulting.net>
2021-06-17 18:51:07 +0200

    Ajout active account + send SMS

--- server/controllers/authCtrl.ts
+++ server/controllers/authCtrl.ts
@@ -4,10 +4,12 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { generateActiveToken, generateAccessToken, generateRefreshToken } from '../config/generateToken'
 import sendMail from '../config/sendMail'
-import { validEmail } from '../middlewares/valid'
+import sendSms from '../config/sendSMS'
+import { validEmail, validPhone } from '../middlewares/valid'
+import { IDecodedToken } from '../config/interfaces'
 
 const authCtrl = {
-    register: async(req: Request, res: Response) => {
+    register: async (req: Request, res: Response) => {
         try {
             const { name, account, password } = req.body
             const user = userModel.findOne({account})
@@ -18,25 +20,38 @@ const authCtrl = {
 
             const newUser = { name, account, password: passwordhash }
 
-            const active_token = generateActiveToken(newUser)
+            const active_token = generateActiveToken({newUser})
 
             const url = `${process.env.BASE_URL}/active/${active_token}`
 
             if (validEmail(account)) {
-                // sendMail(account, "Active your account", url)
+                if (process.env.SEND) {
+                    sendMail(account, "Active your account", url)
+                }
             
-                res.json({ msg: 'Bravo ! Activez votre compte en cliquant sur le lien qui est dans le mail que vous avez reçu.' })
-            }
+                return res.json({ 
+                    msg: 'Bravo ! Activez votre compte en cliquant sur le lien qui est dans le mail que vous avez reçu.',
+                    active_token
+                })
+            } else if (validPhone(account)) {
+                if (process.env.SEND) {
+                    sendSms(account, url, "Verify your phone number")
+                }
+                
+                return res.json({ msg: "Success! Please check phone." })
+              }
         } catch (error) {
             return res.status(500).json({msg: error.message})
         }
     },
-    login: async(req: Request, res: Response) => {
+    login: async (req: Request, res: Response) => {
         try {
             const { account, password } = req.body
-            const user = userModel.findOne({account})
+            const user = await userModel.findOne({account})
 
             if(!user) return res.status(400).json({msg: "Ce compte n'existe pas."})
+
+            console.log(account, password, user)
 
             const isMatch = await bcrypt.compare(password, user.password)
 
@@ -54,8 +69,66 @@ const authCtrl = {
             res.json({
                 msg: "Login Success!",
                 access_token,
-                user
+                user: {...user._doc, password: ''}
             })
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
+    },
+    activeAccount: async(req: Request, res: Response) => {
+        try {
+            const { active_token } = req.body
+    
+            const decoded = <IDecodedToken>jwt.verify(active_token, `${process.env.ACTIVE_TOKEN_SECRET}`)
+
+            const { newUser } = decoded 
+    
+            if(!newUser) return res.status(400).json({msg: "Invalid authentication."})
+            
+            const user = new userModel(newUser)
+
+            await user.save()
+
+            res.json({msg: "Account has been activated!"})
+    
+        } catch (err) {
+            let errMsg;
+    
+            if (err.code === 11000) {
+                errMsg = Object.keys(err.keyValue)[0] + " already exists."
+            } else {
+                let name = Object.keys(err.errors)[0]
+                errMsg = err.errors[`${name}`].message
+            }
+    
+            return res.status(500).json({msg: errMsg})
+        }
+    },
+    logout: async (req: Request, res: Response) => {
+        try {
+            res.clearCookie('refreshtoken', { path: `/api/refresh_token` })
+            return res.json({msg: "Logout !"})
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
+    },
+    refreshToken: async (req: Request, res: Response) => {
+        try {
+            const rf_token = req.cookies.refreshtoken
+
+            if (!rf_token) return res.status(500).json({ msg: "Merci de vous authentifier !" })
+
+            const decoded = <IDecodedToken>jwt.verify(rf_token, `${process.env.REFRESH_TOKEN_SECRET}`)
+
+            if (!decoded) return res.status(500).json({ msg: "Merci de vous authentifier !" })
+
+            const user = await userModel.findById(decoded.id).select('-password')
+
+            if (!user) return res.status(500).json({ msg: "Ce compte n'existe pas !" })
+
+            const access_token = generateAccessToken({id: user._id})
+
+            res.json({msg: 'Success'})
         } catch (error) {
             return res.status(500).json({ msg: error.message })
         }
--- server/routes/authRouter.ts
+++ server/routes/authRouter.ts
@@ -6,6 +6,12 @@ const Router = express.Router()
 
 Router.post('/register', validRegister, authCtrl.register)
 
+Router.post('/active', authCtrl.activeAccount)
+
 Router.post('/login', authCtrl.login)
+
+Router.get('/logout', authCtrl.logout)
+
+Router.get('/refresh_token', authCtrl.refreshToken)
 
 export default Router
